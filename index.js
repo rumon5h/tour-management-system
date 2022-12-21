db3a0fa2726c667ac89b8f61c3938413aa7303c4
rumon5h <mdrumonkhan48519@gmail.com>
2022-12-21 19:14:08 +0600

    fixed one issue

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+// Database configuration
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log('Successfully connected the database');
+})
 
 // Home route
 app.get('/', (req, res, next) => {
@@ -16,5 +24,10 @@ const tourRoutes = require('./routes/tour.route');
 
 
 app.use('/api/v1/tour', tourRoutes)
+
+
+app.listen(PORT, () => {
+    console.log('Server is running at ', PORT);
+})
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-const app = require('./index.js');
-const PORT = process.env.PORT || 5000;
 
-// Database configuration
-mongoose.connect(process.env.DATABASE).then(() => {
-    console.log('Successfully connected the database');
-})
 
-app.listen(PORT, () => {
-    console.log('Server is running at ', PORT);
-})
+
